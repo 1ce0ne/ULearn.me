d47adf7665f680627e429cc5e56500031f7e04d6
Aleksandr Chernakov <alexandertschernakow@gmail.com>
2022-05-20 00:59:57 +0300

    Решение задачи: Создание классов

---
+++ ULearn/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Test1
+{
+	public class GeoLocation
+	{
+		public double Latitude;
+		public double Longitude;
+	}
+	public class City
+	{
+		public string Name;
+		public GeoLocation Location;
+	}
+
+	public class Program
+	{
+		static void Main()
+		{
+			var city = new City();
+			city.Name = "Ekaterinburg";
+			city.Location = new GeoLocation();
+			city.Location.Latitude = 56.50;
+			city.Location.Longitude = 60.35;
+			Console.WriteLine("I love {0} located at ({1}, {2})",
+				city.Name,
+				Convert.ToString(city.Location.Longitude),
+				Convert.ToString(city.Location.Latitude));
+		}
+	}
+}
